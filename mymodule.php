b0ca1950afafb8e5a0096d83007f389a3ae22e91
Hubert Suchorowski <hubert.suchorowski1@gmail.com>
2024-11-29 10:42:01 +0100

    Initial mymodule

---
+++ mymodule.php
@@ -0,0 +1,43 @@
+<?php
+if (!defined('_PS_VERSION_')) {
+    exit;
+}
+
+class MyModule extends Module
+{
+    public function __construct()
+    {
+        $this->name = 'mymodule';
+        $this->tab = 'front_office_features';
+        $this->version = '0.1.0';
+        $this->author = 'Hubert Suchorowski';
+        $this->need_instance = 1;
+        parent::__construct();
+
+        $this->displayName = $this->trans('My module', [], 'Modules.MyModule.Admin');
+        $this->description = $this->trans('Description of my module.', [], 'Modules.MyModule.Admin');
+        $this->confirmUninstall = $this->trans('Are you sure you want to uninstall?', [], 'Modules.MyModule.Admin');
+    }   
+
+    public function install()
+    {
+        if (Shop::isFeatureActive()) {
+            Shop::setContext(Shop::CONTEXT_ALL);
+    }
+
+        return (
+            parent::install() 
+            && Configuration::updateValue('MYMODULE_NAME', 'my module')
+        ); 
+    }
+
+    public function uninstall()
+    {
+        return (
+            parent::uninstall() 
+            && Configuration::deleteByName('MYMODULE_NAME')
+        );
+    }
+
+}
+
